ebcef5599b1b82a03a155413397b093290c95ae9
keshabbhattarai126 <keshabbhattarai126@gmail.com>
2024-11-25 07:54:38 +0545

    repo model project first commit

---
+++ repo_soft/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace repo_soft.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Address { get; set; }
+    public string  PhoneNumber {get; set; }
+    public string Email {  get; set; }
+    public DateTime ENrollmentDate {  get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string CreatedBy { get; set; }
+
+
+}
